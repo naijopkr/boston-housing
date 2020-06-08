6280db1669622937d178f94c56472d6bbeeeb5cf
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-06-08 15:06:32 -0500

    style fix

--- __main__.py
+++ __main__.py
@@ -44,7 +44,12 @@ def main():
     X = df[df.columns]
     y = df.index
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+    X_train,X_test,y_train,y_test = train_test_split(
+        X,
+        y,
+        test_size=0.4,
+        random_state=101
+    )
 
     lm = LinearRegression()
     lm.fit(X_train, y_train)
